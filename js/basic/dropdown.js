03754c01aca12184494f742a225c4553571ecaa8
GlebGroskreits <0Gleb.Groskreits0@gmail.com>
2024-05-27 15:26:20 +0300

    feat: add active dropdown

---
+++ js/basic/dropdown.js
@@ -0,0 +1,50 @@
+const drop = document.querySelector('.dropdown');
+
+const burger = document.querySelector('.burger-menu');
+const burgerlog = burger.querySelector('img');
+
+const headerfunctional = document.querySelector('.header-functional');
+const imgheaderfun = document.querySelectorAll('img');
+const imglogout = imgheaderfun[2];
+
+const pdropdown = drop.querySelectorAll('p');
+
+const actuser = localStorage.getItem('ActiveUsser');
+
+if(actuser == ''){
+    imglogout.addEventListener('click', sign);
+    burgerlog.addEventListener('click', sign);
+}else{
+    imglogout.addEventListener('click', show);
+    burgerlog.addEventListener('click', show);
+}
+
+
+pdropdown[2].addEventListener('click', sign);
+pdropdown[4].addEventListener('click', show);
+pdropdown[5].addEventListener('click', logoutdrop);
+
+function show(){
+    const activeUserString = localStorage.getItem('ActiveUsser');
+    const activeU = activeUserString ? JSON.parse(activeUserString) : null;
+
+    if (activeU) {
+        drop.classList.toggle('active');
+    }
+    else{
+       location.href = "../pages/home.html"
+    }
+}
+
+function sign(){
+    location.href = "../pages/sign_in.html"
+}
+
+function logoutdrop(){
+    localStorage.setItem('ActiveUsser', '');
+    location.reload();
+}
+
+window.addEventListener('scroll', () => {
+    drop.classList.remove('active');
+});
